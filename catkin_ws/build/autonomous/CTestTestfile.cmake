4b2716a34bf0098b659c90aa21f40ed405afe877
liubangya <liubangya@gmail.com>
2020-10-29 16:02:33 -0500

    gitignore

--- catkin_ws/build/autonomous/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sxue8/catkin_ws/src/autonomous
-# Build directory: /home/sxue8/catkin_ws/build/autonomous
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
